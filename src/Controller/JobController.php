b6257bf52cde0741797cf937e7cb85befd42b447
Martial Mendes <martial.mendes@gmail.com>
2024-09-23 17:02:26 +0200

    afficher une offre

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Offre;
 use App\Repository\OffreRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class JobController extends AbstractController
 {
@@ -14,6 +15,14 @@ class JobController extends AbstractController
     {
         return $this->render('job/index.html.twig', [
             'offres'=>$offreRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_job_show')]
+    public function show(Offre $offre): Response
+    {
+        return $this->render('job/show.html.twig', [
+            'offre'=>$offre,
         ]);
     }
 }
